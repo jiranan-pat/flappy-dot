0e4c514cec8dd1129964b53a5b336359b7b6ec75
jiranan-pat <jiranan.pat@ku.th>
2019-02-21 15:11:59 +0700

    Edited flappy.py and added models.py

--- flappy.py
+++ flappy.py
@@ -1,7 +1,23 @@
 import arcade
+from models import World
  
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+ 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+ 
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+ 
  
 class FlappyDotWindow(arcade.Window):
     def __init__(self, width, height):
@@ -9,8 +25,18 @@ class FlappyDotWindow(arcade.Window):
  
         arcade.set_background_color(arcade.color.WHITE)
  
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+ 
+        self.dot_sprite = ModelSprite('images/dot.png', model=self.world.player)
+ 
+    def update(self, delta):
+        self.world.update(delta)
+ 
     def on_draw(self):
         arcade.start_render()
+ 
+        self.dot_sprite.draw()
+
  
  
 def main():
